a25c4880bb4eef01d822cdfcc4bdf8448aec539b
Ewen Le Bihan <hey@ewen.works>
2021-05-06 16:44:59 +0200

    rename edge detection batch file & add orig image

--- generate_edge_detection_batch.py
+++ generate_edge_detection_batch.py
@@ -14,17 +14,18 @@ from rich.progress import Progress
 
 
 batchesdir = Path("edge-detection/batches")
-def detect_edges(filename: str,  low: int, high: int, σ: int=3):
+def detect_edges(filename: Path,  low: int, high: int, σ: int=3):
     σ, low, high = map(int, (σ, low, high))
 
-    image = cv2.imread(filename, 0)
+    image = cv2.imread(str(filename), 0)
     edges = cv2.Canny(image, low, high, apertureSize=σ, L2gradient=True)
     return image, edges
 
 
 
 
-tries = {"start": int(sys.argv[1]), "stop": int(sys.argv[2]), "step": int(sys.argv[3])}
+tries = {"start": int(sys.argv[2]), "stop": int(sys.argv[3]), "step": int(sys.argv[4])}
+infile = Path(sys.argv[1])
 total_steps = len(range(*tries.values()))**2
 print(f"[red]cleaning up")
 rmtree('edge-detection/batches')
@@ -36,16 +37,19 @@ with Progress() as progress_bar:
     for high in range(*tries.values()):
         for low in range(*tries.values()):
             plt.suptitle(f"σ={3}, high={high}, low={low}, high×low={high*low}, \nfile is datasets/various/treated/*coude_clair_2.png")
-            for i, filename in enumerate([ 'treated/coude_clair_2.png', 'treated/baseline_coude_clair_2.png' ]):
-                _, edges = detect_edges(f"datasets/various/{filename}", low, high)
+            for i, filename in enumerate([infile, infile.parent / f"baseline_{infile.name}"]):
+                _, edges = detect_edges(filename, low, high)
                 # plt.subplot(140 + i)
                 # plt.title(filename)
                 # plt.imshow(img, cmap='gray')
                 # i += 1
                 # plt.subplot(140 + i)
-                plt.subplot(121 + i)
-                plt.title(f"Edges of {'healed' if 'baseline' in filename else 'broken'} bone")
+                plt.subplot(131 + i)
+                plt.title(f"Edges of {'healed' if 'baseline' in str(filename) else 'broken'} bone")
                 plt.imshow(edges, cmap='gray')
+            plt.subplot(133)
+            plt.title(f"Original image")
+            plt.imshow(cv2.imread(str(infile), 0))
             out_filename = f"edge-detection/batches/{low*high:0{len(str(int(sys.argv[2])**2))}d} low={low:03d} high={high:03d}.png"
             plt.savefig(out_filename)
             progress_bar.update(task, advance=1, description=f"did [yellow]low[/]=[cyan bold]{low:03d}[/] [yellow]high[/]=[cyan bold]{high:03d}[/]")
