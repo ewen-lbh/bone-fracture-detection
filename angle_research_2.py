0a2c90a393ce997c1dd4e881e51011bb911c15b0
Ewen Le Bihan <hey@ewen.works>
2021-06-18 09:01:15 +0200

    hough line transform properly

---
+++ angle_research_2.py
@@ -0,0 +1,36 @@
+import pathlib
+from typing import Optional
+import numpy as np
+
+from skimage.transform import hough_line, hough_line_peaks, probabilistic_hough_line
+
+import matplotlib.pyplot as plt
+
+
+def get_lines(edges: np.ndarray) -> list[tuple[int, int, float]]:
+    """
+    Returns list of lines: (x, y, angle)
+    """
+    tested_angles = np.linspace(-np.pi / 2, np.pi / 2, 90, endpoint=False)
+    lines = []
+    h, theta, d = hough_line(edges, theta=tested_angles)
+
+    for _, angle, dist in zip(*hough_line_peaks(h, theta, d)):
+        (x0, y0) = dist * np.array([np.cos(angle), np.sin(angle)])
+        lines.append((x0, y0, np.tan(angle + np.pi/2)))
+
+    return lines
+
+def display_lines(image: np.ndarray, lines: list[tuple[int, int, float]], save: Optional[pathlib.Path] = None):
+    """
+    Display lines on top of image with matplotlib
+    """
+    plt.imshow(image, cmap="gray")
+    for x, y, slope in lines:
+        plt.axline((x, y), slope=slope)
+        plt.scatter(x, y)
+
+    if save:
+        plt.savefig(str(save))
+    else:
+        plt.show()
--- detect.py
+++ detect.py
@@ -1,3 +1,4 @@
+from angle_research_2 import display_lines, get_lines
 from pathlib import Path
 from svgpathtools import svg2paths
 from edge_detection_batch import detect_edges
@@ -6,6 +7,7 @@ import cv2
 import potrace
 import numpy as np
 import matplotlib.pyplot as plt
+from rich.progress import Progress
 
 def vectorize(image):
     # convert cells of 3-valued lists (RGB) to a single value (greyscale)
@@ -22,11 +24,23 @@ def is_broken(edges: list[list[float]]) -> bool:
 def is_white(pixel: float) -> bool:
     return pixel > 0.75
 
+def center_of(image: np.ndarray) -> np.ndarray:
+    # return image[:, len(image[0])//4: -len(image[0])//4]
+    return image
 
-orig, edges = detect_edges(Path("datasets/various/coude_clair_2.png"), low=40, high=120)
-center = edges[:, len(edges[0]) // 4: -len(edges[0]) // 4 ]
-cv2.imwrite("datasets/various/coude_clair_2_edges.png", center)
-# convert -resize 500% -filter gaussian -blur 0x4 -modulate 180 coude_clair_2.png coude_clair_2_edges.bmp
-# potrace --svg coude_clair_2_edges.bmp
-paths, _ = svg2paths("datasets/various/coude_clair_2_edges.svg")
-print(paths)
+def save_figure(image: Path):
+    _, edges = detect_edges(image, low=40, high=120)
+    lines = get_lines(center_of(edges))
+    display_lines(center_of(edges), lines, save=Path("line-detection") / image.name)
+
+with Progress() as bar:
+    files = list(Path("datasets/various").glob("*.png"))
+    task = bar.add_task("[blue]Processing", total=len(files))
+    for testfile in files:
+        save_figure(testfile)
+        bar.advance(task)
+# cv2.imwrite("datasets/various/coude_clair_2_edges.png", center)
+# # convert -resize 500% -filter gaussian -blur 0x4 -modulate 180 coude_clair_2.png coude_clair_2_edges.bmp
+# # potrace --svg coude_clair_2_edges.bmp
+# paths, _ = svg2paths("datasets/various/coude_clair_2_edges.svg")
+# print(paths)
