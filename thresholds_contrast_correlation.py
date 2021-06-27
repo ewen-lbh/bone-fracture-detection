b32b0466a880f8873fd75ca4bc696e394d5ea779
Ewen Le Bihan <hey@ewen.works>
2021-06-27 17:58:09 +0200

    correlation: generate graphs in french

--- thresholds_contrast_correlation.py
+++ thresholds_contrast_correlation.py
@@ -18,11 +18,16 @@ attr = lambda name: list(map((lambda o: o.get(name, 0)), data_as_list))
 
 fig, axes = plt.subplots(1, 2)
 
+french = {
+        "contrast": "du constraste",
+        "bright": "de la luminosité",
+}
+
 for current_axis, varying in enumerate(("contrast", "bright")):
-    data_as_list.sort(key=partial(access, key=varying))
-    axes[current_axis].set_title(f"Varying {varying}")
-    axes[current_axis].plot(attr(varying), attr("hi"), label="hi", color="red")
-    axes[current_axis].plot(attr(varying), attr("lo"), label="lo", color="skyblue")
+    data_as_list.sort(key=lambda o: o[varying])
+    axes[current_axis].set_title(f"En fonction {french[varying]}")
+    axes[current_axis].plot(attr(varying), attr("hi"), label="seuil haut", color="red")
+    axes[current_axis].plot(attr(varying), attr("lo"), label="seuil bas", color="skyblue")
     axes[current_axis].legend()
 
 plt.show()
